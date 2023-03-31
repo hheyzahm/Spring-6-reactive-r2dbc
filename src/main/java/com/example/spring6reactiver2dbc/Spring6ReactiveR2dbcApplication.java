ecad763fdc3c7ba4727f9b44a96d2c37f4ff545e
Hazeem Hassan <hhazeemhassan@gmail.com>
2023-03-31 13:59:19 +0500

    Initial commit

---
+++ src/main/java/com/example/spring6reactiver2dbc/Spring6ReactiveR2dbcApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring6reactiver2dbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6ReactiveR2dbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6ReactiveR2dbcApplication.class, args);
+    }
+
+}
