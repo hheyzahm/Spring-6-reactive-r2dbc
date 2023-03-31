a454e1c7969324f440e9bf422f490aa740953330
Hazeem Hassan <hhazeemhassan@gmail.com>
2023-03-31 15:22:38 +0500

    Using Path Variables

--- src/main/java/com/example/spring6reactiver2dbc/services/BeerService.java
+++ src/main/java/com/example/spring6reactiver2dbc/services/BeerService.java
@@ -2,6 +2,7 @@ package com.example.spring6reactiver2dbc.services;
 
 import com.example.spring6reactiver2dbc.model.BeerDTO;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 /**
  * @Created 31 03 2023 - 3:08 PM
@@ -10,4 +11,5 @@ import reactor.core.publisher.Flux;
 public interface BeerService {
 
     Flux<BeerDTO> listBeer();
+    Mono<BeerDTO> getBeerById(Integer beerId);
 }
--- src/main/java/com/example/spring6reactiver2dbc/services/BeerServiceImpl.java
+++ src/main/java/com/example/spring6reactiver2dbc/services/BeerServiceImpl.java
@@ -6,6 +6,7 @@ import com.example.spring6reactiver2dbc.repository.BeerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 /**
  * @Created 31 03 2023 - 3:09 PM
@@ -20,13 +21,31 @@ public class BeerServiceImpl implements BeerService {
     @Override
     public Flux<BeerDTO> listBeer() {
         return beerRepository.findAll().map(beerMapper::beerToBeerDto);
-
     }
 
-
-
-
-
-
-
+    @Override
+    public Mono<BeerDTO> getBeerById(Integer beerId) {
+        return beerRepository.findById(beerId).map(beerMapper::beerToBeerDto);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
