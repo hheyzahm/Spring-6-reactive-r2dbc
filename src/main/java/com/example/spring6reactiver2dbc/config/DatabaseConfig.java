4d110ada9fa43c892aecef941a99c3aa0775d9b3
Hazeem Hassan <hhazeemhassan@gmail.com>
2023-03-31 14:04:28 +0500

    Initailize Database

---
+++ src/main/java/com/example/spring6reactiver2dbc/config/DatabaseConfig.java
@@ -0,0 +1,29 @@
+package com.example.spring6reactiver2dbc.config;
+
+import io.r2dbc.spi.ConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
+
+/**
+ * @Created 31 03 2023 - 2:01 PM
+ * @Author Hazeem Hassan
+ */
+@Configuration
+public class DatabaseConfig {
+
+    @Value("classpath:/schema.sql")
+    Resource resource;
+
+    @Bean
+    ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory){
+        ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
+        initializer.setConnectionFactory(connectionFactory);
+        initializer.setDatabasePopulator(new ResourceDatabasePopulator(resource));
+        return initializer;
+    }
+}
+
